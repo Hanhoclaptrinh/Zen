4d459d8cd155dc46d137a15a2bcd3cc254b06afb
levinhhan <pitou.psy@gmail.com>
2025-12-29 23:26:49 +0700

    feat(categories): implement CRUD operations for categories with JWT authentication

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import type { AuthRequest } from 'src/common/types/auth-request';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(private readonly categoriesService: CategoriesService) { }
+
+    @Get()
+    findAll(@Req() req: AuthRequest) {
+        const userId = req.user.id;
+        return this.categoriesService.findAll(userId);
+    }
+
+    @Post()
+    create(@Body() dto: CreateCategoryDto, @Req() req: AuthRequest) {
+        const userId = req.user.id;
+        return this.categoriesService.create(dto, userId);
+    }
+
+    @Put(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateCategoryDto, @Req() req: AuthRequest) {
+        const userId = req.user.id;
+        return this.categoriesService.update(id, dto, userId);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number, @Req() req: AuthRequest) {
+        const userId = req.user.id;
+        return this.categoriesService.remove(id, userId);
+    }
+}
--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(private prisma: PrismaService) { }
+
+    findAll(userId: number) {
+        return this.prisma.category.findMany({
+            where: { userId }
+        });
+    }
+
+    create(dto: CreateCategoryDto, userId: number) {
+        return this.prisma.category.create({
+            data: {
+                name: dto.name,
+                type: dto.type,
+                userId
+            }
+        });
+    }
+
+    update(id: number, dto: UpdateCategoryDto, userId: number) {
+        return this.prisma.category.update({
+            where: { id, userId },
+            data: dto
+        })
+    }
+
+    remove(id: number, userId: number) {
+        return this.prisma.category.delete({
+            where: { id, userId }
+        });
+    }
+}
